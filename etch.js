357ee060b73a38814a7f180c6917745929dc24f3
JigarSwam <jswaminarayan@gmail.com>
2025-02-15 10:14:42 -0800

    create pixelate effect

--- etch.js
+++ etch.js
@@ -1,6 +1,6 @@
 container = document.querySelector(".container");
 
-let startingBoxCount = 20;
+let startingBoxCount = 10;
 let totalBoxes = startingBoxCount * startingBoxCount;
 
 const canvasWidth = container.clientWidth;
@@ -11,12 +11,26 @@ let createCanvas = () => {
     for (let i = 0; i < totalBoxes; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.setAttribute("class", "gridSquare");
-        gridSquare.style.width = `${Math.floor((startingBoxCount / totalBoxes) * 100)}%`;
-        gridSquare.style.height = `${Math.floor((startingBoxCount / totalBoxes) * 100)}%`;
+        gridSquare.style.width = `${(startingBoxCount / totalBoxes) * 100}%`;
+        gridSquare.style.height = `${(startingBoxCount / totalBoxes) * 100}%`;
         gridSquare.textContent = `${i}`
         container.append(gridSquare);
     }
 }
 
 createCanvas();
+
+// Hovering effect
+// when mouse hovers over a div
+// change color of div to black
+let pixelate = () => {
+    let allBoxes = document.querySelectorAll(".gridSquare");
+    allBoxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.style.background = "black";
+        })
+    })
+};
+
+pixelate();
 
